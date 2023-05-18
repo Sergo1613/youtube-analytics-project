dbcd0091bde2179fd98190f53bf2840fdad7e840
Sergo1613 <mentoss2012@gmail.com>
2023-05-18 23:04:02 +0300

    Homework 3

--- src/channel.py
+++ src/channel.py
@@ -22,6 +22,33 @@ class Channel:
         self.view_count = self.channel_info["items"][0]["statistics"].get("viewCount")
 
 
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __gt__(self, other):
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __ge__(self, other):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+    def __lt__(self, other):
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __le__(self, other):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+
+
+
+
+
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
